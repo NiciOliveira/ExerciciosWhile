bd375efe1e2b2e78a92a6c6bb72d64b4416eafe4
Itaici <Itaici.oliveira96@hotmail.com>
2020-07-21 10:28:08 -0300

    Alterações

--- src/exercicios/exercicio01.java
+++ src/exercicios/exercicio01.java
@@ -1,8 +1,4 @@
-/* Exercicio 01
- * Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
- * incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
- * impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
- */
+
 package exercicios;
 
 import java.util.Scanner;
--- src/exercicios/exercicio03.java
+++ src/exercicios/exercicio03.java
@@ -1,18 +1,39 @@
-/*Exercicio 03
- * Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
- * um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
- * 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
- * que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
- * mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
- * exemplo.
-*/
-
 package exercicios;
+
+import java.util.Scanner;
 
 public class exercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int numPreferencia;
+		int alcool = 0;
+		int gasolina = 0;
+		int disel = 0;
+		
+		System.out.println("Votação de preferenia");
+		System.out.println("");
+		numPreferencia = scan.nextInt();
+		
+		while(numPreferencia != 4) {
+			if(numPreferencia == 1) {
+				alcool+=1;
+				System.out.println("Alcool: " + alcool);
+			} else if(numPreferencia == 2) {
+				gasolina+=1;
+				System.out.println("Gasolina: " + gasolina);
+			}else if(numPreferencia == 3) {
+				disel+=1;
+				System.out.println("Disel: " + disel);
+			}else {
+				System.out.println("Código inválido!");
+			}
+			System.out.println("Infome seu voto: ");
+			numPreferencia = scan.nextInt();
+		}
+		System.out.println("MUITO OBRIGADA");
+		scan.close();
 
 	}
 
