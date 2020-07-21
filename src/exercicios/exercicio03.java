973e979951e461fa0b2db58036d12e4c826b8035
Itaici <Itaici.oliveira96@hotmail.com>
2020-07-21 09:57:06 -0300

    inclusão de exercício 03 e atualização do documento readme

---
+++ src/exercicios/exercicio03.java
@@ -0,0 +1,19 @@
+/*Exercicio 03
+ * Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva
+ * um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel
+ * 4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até
+ * que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a
+ * mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme
+ * exemplo.
+*/
+
+package exercicios;
+
+public class exercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
